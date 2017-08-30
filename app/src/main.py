d067a4746d6ca4214b02fabbc739430ce37e3419
Hossein <hossein.seyedmehdi@gmail.com>
2017-08-30 07:14:43 -0400

    Unsuccessful attempt to use the phantomjs

--- app/src/main.py
+++ app/src/main.py
@@ -4,17 +4,17 @@ Created on Aug 29, 2017
 @author: Hossein
 '''
 
+from selenium import webdriver
 
-from requests import Session
+url_main_team_page = "https://secure.e2rm.com/registrant/TeamFundraisingPage.aspx?teamID=738302&langPref=en-CA"
+url_ajax_request = "https://secure.e2rm.com/registrant/WebServices/RegistrantWebService.asmx/GetTeamMembers"
 
 
-def get_data_from_web():
+def ajax_request():
   '''
   Get the data from the web. This tries to immitate the 'function getTeamMembers() {' in the 
   source code of the website here: view-source:https://secure.e2rm.com/registrant/TeamFundraisingPage.aspx?teamID=738302&langPref=en-CA 
   '''
-  name_money_dict = {}
-  session = Session()
   
   '''
   var teamId = parseInt($teamListContainer.attr("data-team-id"));
@@ -23,41 +23,42 @@ def get_data_from_web():
   var anonymousText = $teamListContainer.find(".anonymous-team-member").text();
   var isOrderByAmount = $teamListContainer.attr("data-order-by-amount") == "True";
   '''
-  
-  team_page_url = "https://secure.e2rm.com/registrant/TeamFundraisingPage.aspx?teamID=738302&langPref=en-CA"
-  ajax_call_url = "https://secure.e2rm.com/registrant/WebServices/RegistrantWebService.asmx/GetTeamMembers"
-  teamId = "738302"
-  languageCode = "en-CA"
-  sourceReferrerUrl = "https://secure.e2rm.com/registrant/search.aspx?eventid=210107&langpref=en-CA"
-  anonymousText = "Anonymous"
-  isOrderByAmount = "true"
-  
-  print(session.cookies.get_dict())
-  r = session.get(team_page_url)
-  print(r.headers)
-  print(session.cookies.get_dict())
-  
-  return 
 
-  response = session.post(
-    url = ajax_call_url,
-    data = {'teamID': teamId, 
-            'languageCode': languageCode, 
-            'sourceReferrerUrl': sourceReferrerUrl, 
-            'anonymousText': anonymousText, 
-            'isOrderByAmount': isOrderByAmount}, 
-    headers = r.headers)
+  from requests import Session
+  import json
+  
+  url_main_team_page = "https://secure.e2rm.com/registrant/TeamFundraisingPage.aspx?teamID=738302&langPref=en-CA"
+  url_ajax_request = "https://secure.e2rm.com/registrant/WebServices/RegistrantWebService.asmx/GetTeamMembers"
+  
+  session = Session()
+  
+  #print(session.cookies.get_dict())
+  session.get(url_main_team_page)
+  #print(session.cookies.get_dict())
+  
+  payload = json.dumps({'teamID':738302,
+                        'languageCode':'en-CA',
+                        'sourceReferrerUrl':'https://secure.e2rm.com/registrant/search.aspx?eventid=210107&langpref=en-CA',
+                        'anonymousText':'Anonymous',
+                        'isOrderByAmount':'true'})
+  
+  response = session.post(url = url_ajax_request, data = payload)
    
   print(response.text)
   
   return response 
 
+def headless_request():
+  driver = webdriver.PhantomJS()
+  driver.get(url_main_team_page)
+  r = driver.page_source()
+  r.find("Johanna") 
+
 if __name__ == "__main__": 
-  response = get_data_from_web()
-  #print(response)
-  
-  
-  
+  #headless_request()
+  ajax_request()
+
+
 #ASP.NET_SessionId=kfage2wf4qnazbeh30g2dnt2; BIGipServersecure.e2rm.com=1130647562.20480.0000
 #kfage2wf4qnazbeh30g2dnt2
 #1130647562.20480.0000
